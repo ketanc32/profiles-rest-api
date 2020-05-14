d9f22885efaeda631e575a65d62b8e62f4108aaf
ketan <ketanc32@gmail.com>
2020-05-14 22:57:48 +0530

    Added custom user profile model, manager and migrations

--- profiles_api/models.py
+++ profiles_api/models.py
@@ -31,6 +31,7 @@ def create_superuser(self,email,name,password):
     return user
 
 
+
 class UserProfile(AbstractBaseUser,PermissionsMixin):
     """Database model for users in the system"""
     email = models.EmailField(max_length=255,unique=True)
